08f37388129bb2c464b2e38d21fb08a6ca6be482
Brad Philips <brad.t.philips@gmail.com>
2013-08-25 19:39:16 -0400

    Updates for sizing scrollview.

--- DAPagesContainer/DAPagesContainer.m
+++ DAPagesContainer/DAPagesContainer.m
@@ -78,7 +78,6 @@
                                                                      self.topBarHeight,
                                                                      CGRectGetWidth(self.view.frame),
                                                                      CGRectGetHeight(self.view.frame) - self.topBarHeight)];
-    self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
     self.scrollView.delegate = self;
     self.scrollView.pagingEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
