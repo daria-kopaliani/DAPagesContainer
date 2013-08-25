2fed8ec7f3df32c3b5612f35266137d56e3450e1
Brad Philips <brad.t.philips@gmail.com>
2013-08-25 18:38:59 -0400

    Need this one...

--- DAPagesContainer/DAPagesContainer.m
+++ DAPagesContainer/DAPagesContainer.m
@@ -78,6 +78,7 @@
                                                                      self.topBarHeight,
                                                                      CGRectGetWidth(self.view.frame),
                                                                      CGRectGetHeight(self.view.frame) - self.topBarHeight)];
+    self.scrollView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
     self.scrollView.delegate = self;
     self.scrollView.pagingEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
